751324d0353d246cae5ee7444525c82fbffb27da
lottemarines <lottemarines3@gmail.com>
2019-09-04 07:09:55 +0000

    20190904_weather-api-called

--- Modules/DayComponent.js
+++ Modules/DayComponent.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, Image, Dimensions } from 'react-native';
 
+// <Text style={styles.date}>{`${date} (${dateLabel})`}</Text>
+// <Image style={styles.image} source={{uri: image.url}} />
+// <View style={styles.dayTemperature}>
+//   <Text style={styles.dayTemperatureHigh}>
+// 	{ temperature.max? `${temperature.max.celsius}℃` : `-℃` }
+//   </Text>
+//   <Text style={styles.dayTemperatureLow}>
+// 	{ temperature.min? `${temperature.min.celsius}℃` : `-℃` }
+//   </Text>
+// </View>
+
 export default class DayComonent extends Component<Props> {
   render() {
-    const { date, dateLabel, telop, temperature, image } = this.props.weather
+    const { description, icon, id, main, image } = this.props.weather
+	const { temp_max, temp_min, temp, humidity, pressure } = this.props.temp
     return (
       <View style={styles.dayComonent}>
-        <Text style={styles.date}>{`${date} (${dateLabel})`}</Text>
-        <Image style={styles.image} source={{uri: image.url}} />
-        <Text style={styles.day_weather}>{telop}</Text>
-        <View style={styles.dayTemperature}>
-          <Text style={styles.dayTemperatureHigh}>
-            { temperature.max? `${temperature.max.celsius}℃` : `-℃` }
-          </Text>
-          <Text style={styles.dayTemperatureLow}>
-            { temperature.min? `${temperature.min.celsius}℃` : `-℃` }
-          </Text>
-        </View>
+        
+        <Text style={styles.day_weather}>{description}</Text>
+		<Image style={styles.image} source={{uri: 'http://openweathermap.org/img/wn/'+icon+'@2x.png'}} />
+		<Text style={styles.day_weather}>現在の気温 {temp} ℃</Text>
+		 <View style={styles.dayTemperature}>
+		   <Text style={styles.dayTemperatureHigh}>
+		 	{ temp_max? `${temp_max}℃` : `-℃` }
+		   </Text>
+			 <Text style={styles.day_weather}> / </Text>
+		   <Text style={styles.dayTemperatureLow}>
+		 	{ temp_min? `${temp_min}℃` : `-℃` }
+		   </Text>
+		</View>	
+        <Text style={styles.dayHumidity}>{humidity}%</Text>
       </View>
     );
   }
@@ -42,8 +57,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   image: {
-    width: imageWidth * 0.3,
-    height: imageHeight * 0.3,
+    // width: imageWidth * 0.3,
+    // height: imageHeight * 0.3,
+	width: imageWidth * 0.5,
+    height: imageHeight * 0.5,
     borderRadius: 30,
   },
   dayTemperature: {
@@ -52,12 +69,17 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   dayTemperatureHigh: {
-    fontSize: 16,
+    fontSize: 32,
     color: '#eb4d4b',
     marginRight: 10,
   },
   dayTemperatureLow: {
-    fontSize: 16,
+    fontSize: 32,
     color: '#3498db'
+  },
+	dayHumidity: {
+    fontSize: 32,
+    color: '#001aff'
   }
+
 });
